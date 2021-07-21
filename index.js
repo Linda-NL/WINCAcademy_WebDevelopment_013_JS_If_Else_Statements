d391c5c36433a289b07d8fd24f02f4d5cbeeb093
Linda <86129384+Linda-NL@users.noreply.github.com>
2021-07-21 12:11:41 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,23 @@
+// deel 1: leeftijdscheck
+const age = 37;
+if (age >= 18) {
+    console.log('Welkom & veel plezier!');
+} else {
+    console.log('Sorry, minderjarigen mogen niet naar binnen.');
+}
+
+// deel 2: Ladies Night
+const isFemale = true;
+if (isFemale == true) {
+    console.log('Tijdens Ladies Night krijg je een gratis cocktail!');
+} else {
+    console.log('Veel plezier op het feest!');
+}
+
+// deel 3: Wie is de bob?
+const driverStatus = 'kees';
+if (driverStatus == 'bob') {
+    console.log('Goede reis!');
+} else {
+    console.log('Zal ik een taxi voor u bellen?');
+}
